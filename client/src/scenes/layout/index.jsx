068b7bc34cf5ff6a86ec666ddd301bbdf2406e2b
Htet <htetnaing.chess@gmail.com>
2023-05-18 11:55:08 +0630

    make navbar user component to not show on mobile

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -24,6 +24,7 @@ const Layout = () => {
       />
       <Box flexGrow={1}>
         <Navbar
+          isNonMobile={isNonMobile}
           user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
