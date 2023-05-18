6826d86b8b979e0543b3250cb79b010d5a038ee5
Htet <htetnaing.chess@gmail.com>
2023-05-18 11:36:46 +0630

    add products page components

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,25 @@ import { themeSettings } from "theme";
 
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
-
+import Products from "scenes/products";
 
 function App() {
-  const mode = useSelector((state) => state.global.mode)
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <CssBaseline/>
+          <CssBaseline />   
           <Routes>
-            <Route element={<Layout/>}>
-              <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
-              <Route path="/dashboard" element={<Dashboard />}/>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
-      
     </div>
   );
 }
