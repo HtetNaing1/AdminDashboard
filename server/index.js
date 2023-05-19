6a7ccdf46d393ab53606e0a75a7015f5b1d7752e
Htet <htetnaing.chess@gmail.com>
2023-05-19 13:57:24 +0630

    add overallstat model

--- server/index.js
+++ server/index.js
@@ -14,8 +14,15 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+} from "./data/index.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 
 //CONFIGURATIONS
 
